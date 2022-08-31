79f622334c02235eb01cf1d88081e57fff29daeb
libai <libai@wisegraph.cn>
2022-08-31 11:50:44 +0800

    Initial commit

---
+++ src/main/java/com/junda/good/JunDaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.junda.good;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunDaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunDaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junda/good/JunDaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junda.good;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunDaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
